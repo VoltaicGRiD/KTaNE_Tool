c0a41ed5b8eb28612284dd6688af7de64606815b
Dustin Conlon <dustin@dustinconlon.com>
2025-07-18 00:56:38 -0600

    Initial commit

---
+++ KTaNE Tool/vite.config.ts
@@ -0,0 +1,40 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'KTaNE Tool',
+      short_name: 'KTaNE Tool',
+      description: 'A simple tool to help with playing Keep Talking and Nobody Explodes (KTaNE)',
+      theme_color: '#000000',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+})
